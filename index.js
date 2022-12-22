bc7821331e9e0d9913c8c78a2b2c9a418f7833f4
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-12-22 13:16:49 +0600

    create home page api for client side and show courese home page

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express =require('express');
+const express = require('express');
 const cors = require('cors');
 
 const app = express();
@@ -7,22 +7,28 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const courses = require('./data/courses.json');
+const res = require('express/lib/response');
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('skillcrush technology is running');
 })
 
-app.get('/courses/:id', (req, res)=>{
+app.get('/courses/:id', (req, res) => {
     const id = req.params.id;
     const course = courses.find(cours => cours.id == id);
     // console.log(course);
     res.send(course);
 })
 
-app.get('/courses', (req, res)=>{
+app.get('/courses', (req, res) => {
     res.send(courses);
-})
+});
 
-app.listen(port, ()=>{
+// added all in home page // for homecourses
+app.get('/courses', (req, res) => {
+    res.send(courses);
+});
+
+app.listen(port, () => {
     console.log(`Skillcrush Technology runing on port, ${port}`);
 })
